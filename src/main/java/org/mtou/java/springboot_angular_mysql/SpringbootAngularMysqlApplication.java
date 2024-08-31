12ec61dde37cfb4f0cbdef3b1b981a3fa29edbb1
mtouphd <mtou.phd@gmail.com>
2024-08-30 23:59:46 -0400

    initial

---
+++ src/main/java/org/mtou/java/springboot_angular_mysql/SpringbootAngularMysqlApplication.java
@@ -0,0 +1,13 @@
+package org.mtou.java.springboot_angular_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngularMysqlApplication.class, args);
+	}
+
+}
